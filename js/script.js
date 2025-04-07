b946b54692fee5473910a7921228e2615a416300
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-04-07 16:12:04 +0200

    [DONE] Esercizio finito + bonus

--- js/script.js
+++ js/script.js
@@ -27,16 +27,15 @@ const displayCards = array => { // Funzione a cui passo l'array
     return photoCards; // Ritorno la stringa completa
 }
 
-const focusCard = (arr) => {
+const focusCard = (arr) => { // Funzione che mi apre l'immagine in overlay
 
-    arr.forEach(item => {
+    arr.forEach(item => { // Ciclo la nodelist
 
-        item.addEventListener(`click`, () => {
-            let img = item.querySelector(`.photo-img>img`);
-            let overlayImg = overlay.querySelector(`img`);
-            overlay.classList.remove(`d-none`);
-            overlayImg.src = img.src;
-            console.log(overlay);
+        item.addEventListener(`click`, () => { // Aggiungo l'evento all'elemento cliccato
+            let img = item.querySelector(`.photo-img>img`); // Recupero il tag img dal elemento passato (cards)
+            let overlayImg = overlay.querySelector(`img`); // Recupero il tag img da overlay
+            overlay.classList.remove(`d-none`); // Tolgo la classe d-none da overlay
+            overlayImg.src = img.src; // Passo il link dell'immagine
         })
 
     });
@@ -44,21 +43,22 @@ const focusCard = (arr) => {
 
 const closeFocus = () => {
 
-    closeBtn.addEventListener(`click`, () => {
-        overlay.classList.add(`d-none`);
+    closeBtn.addEventListener(`click`, () => { // Aggiungo evento click al button close
+        overlay.classList.add(`d-none`); // Vado ad aggiungere nuovamente la classe d-none
     })
 
 }
 
-let photoSection = document.getElementById(`photo-section`); // Prendo l'elemento dalla pagina HTML
+// Prendo gli elementi necessari dalla pagina HTML
+let photoSection = document.getElementById(`photo-section`);
 let overlay = document.getElementById(`overlay`);
 let closeBtn = document.getElementById(`close`);
 
 axios.get(api).then(resp => { // Effettuo la chiamata AJAX
     photoSection.innerHTML = displayCards(resp.data); // Passo l'array ricevuto dalla chiamata alla funzione displayCards e inserisco il tutto nella photo-section
-    let cards = document.querySelectorAll(`.photo-card`);
+    let cards = document.querySelectorAll(`.photo-card`); // Mi recupero l'elemento con classe photo-card aggiunto prima
 
-    focusCard(cards);
+    focusCard(cards); // richiamo la funzione a cui passo cards
 
-    closeFocus();
+    closeFocus(); // Funzione per chiudere l'overlay
 })
